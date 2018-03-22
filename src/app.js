55f03afc15f0325ae76f175094d4d7c76ace1d97
Dustin Dowell <dustindowell22@gmail.com>
2018-03-22 16:31:13 -0500

    add content

--- src/app.js
+++ src/app.js
@@ -12,10 +12,12 @@ const view = (...args) =>
     h('div', { class: 'app-box' }, [
       h('h1', null, 'Creative Developers'),
       h('h2', null, 'A Discord community for creative developers!'),
-      h('div', null, 'Chat with other developers.'),
-      h('div', null, 'Ask questions and share knowledge.'),
-      h('div', null, 'Find jobs and get feedback.'),
-      h('a', { href: 'https://discord.gg/rkrFHDb' }, 'Let\'s go!')
+      h('div', { class: 'app-list' }, [
+        h('div', null, 'Chat with other developers.'),
+        h('div', null, 'Ask questions and share knowledge.'),
+        h('div', null, 'Find jobs and get feedback.')
+      ]),
+      h('a', { class: 'app-button', href: 'https://discord.gg/rkrFHDb' }, 'Let\'s go!')
     ])
   ])
 
