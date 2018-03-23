d4daaf23fde5c9e3ca11f5009c6a09ae56db0dc2
Dustin Dowell <dustindowell22@gmail.com>
2018-03-23 17:42:48 -0500

    visual updates

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ const actions = {}
 const view = (...args) =>
   h('div', { class: 'app' }, [
     h('div', { class: 'app-box' }, [
-      h('h1', null, 'Creative Developers'),
+      h('h1', null, 'Creative\nDevelopers.io'),
       h('h2', null, 'A Discord community for creative developers!'),
-      h('div', { class: 'app-list' }, [
-        h('div', null, 'Chat with other developers.'),
-        h('div', null, 'Ask questions and share knowledge.'),
-        h('div', null, 'Find jobs and get feedback.')
+      h('ul', { class: 'app-list' }, [
+        h('li', null, 'Chat with other developers.'),
+        h('li', null, 'Ask questions and share knowledge.'),
+        h('li', null, 'Find jobs and get feedback.')
       ]),
-      h('a', { class: 'app-button', href: 'https://discord.gg/rkrFHDb' }, 'Let\'s go!')
+      h('a', { class: 'app-button', href: 'https://discord.gg/rkrFHDb' }, 'Join Now')
     ])
   ])
 
