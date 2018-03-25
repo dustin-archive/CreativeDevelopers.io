8dbb1aeb185a01915a4f90cd858196ec80f5dfb7
Dustin Dowell <dustindowell22@gmail.com>
2018-03-25 03:26:24 -0500

    temporary fix for hyperapp/render replacing quotes in css

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
 
 import fs from 'fs'
 
-import { h, app } from 'hyperapp'
+import { app } from 'hyperapp'
 import { withRender } from '@hyperapp/render'
 
 import App from './src/views/App'
 
-const css = fs.readFileSync('./dist/app.css')
+const css = fs.readFileSync('./dist/app.css', 'utf-8')
 
 const state = {}
 
 const actions = {}
 
-const view = s =>
-  h('html', { lang: 'en-US' }, [
-    h('head', null, [
-      h('meta', { charset: 'utf-8' }),
-      h('title', null, 'Creative Developers'),
-      h('meta', { name: 'google-site-verification', content: 'z7NVwftYsHrQXIO0BJt2hCAK6jP16n-D7DQrmgxLI_I' }),
-      h('meta', { name: 'author', content: 'Dustin Dowell' }),
-      h('meta', { name: 'description', content: 'A Discord community for creative developers!' }),
-      h('meta', { name: 'keywords', content: 'backend, chat, community, creative, developers, discord, frontend, slack' }),
-      h('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=0' }),
-      h('link', { rel: 'icon', type: 'image/png', href: 'favicon.png' }),
-      h('style', null, css),
-      h('script', { defer: true, src: 'app.js' })
-    ]),
-    h('body', null, [
-      h('div', { id: 'app' }, App)
-    ])
-  ])
+const view = s => App
 
-process.stdout.write('<!DOCTYPE html>' + withRender(app)(state, actions, view).toString())
+const render = withRender(app)(state, actions, view).toString()
+
+const html = `<!DOCTYPE html>
+<html lang='en-US'>
+  <head>
+    <meta charset='utf-8'>
+    <title>Creative Developers</title>
+    <meta name='google-site-verification' content='z7NVwftYsHrQXIO0BJt2hCAK6jP16n-D7DQrmgxLI_I'>
+    <meta name='author' content='Dustin Dowell'>
+    <meta name='description' content='A Discord community for creative developers!'>
+    <meta name='keywords' content='backend, chat, community, creative, developers, discord, frontend, slack'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0'>
+    <link rel='icon' type='image/png' href='favicon.png'>
+    <style>${css}</style>
+    <script defer src='app.js'></script>
+  </head>
+  <body>
+    <div id='app'>${render}</div>
+  </body>
+</html>`
+
+// const view = s =>
+//   h('html', { lang: 'en-US' }, [
+//     h('head', null, [
+//       h('meta', { charset: 'utf-8' }),
+//       h('title', null, 'Creative Developers'),
+//       h('meta', { name: 'google-site-verification', content: 'z7NVwftYsHrQXIO0BJt2hCAK6jP16n-D7DQrmgxLI_I' }),
+//       h('meta', { name: 'author', content: 'Dustin Dowell' }),
+//       h('meta', { name: 'description', content: 'A Discord community for creative developers!' }),
+//       h('meta', { name: 'keywords', content: 'backend, chat, community, creative, developers, discord, frontend, slack' }),
+//       h('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=0' }),
+//       h('link', { rel: 'icon', type: 'image/png', href: 'favicon.png' }),
+//       h('style', null, css),
+//       h('script', { defer: true, src: 'app.js' })
+//     ]),
+//     h('body', null, [
+//       h('div', { id: 'app' }, App)
+//     ])
+//   ])
+
+// process.stdout.write('<!DOCTYPE html>' + withRender(app)(state, actions, view).toString())
+
+process.stdout.write(html)
