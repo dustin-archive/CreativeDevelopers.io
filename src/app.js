c6a964083c5b3aed27c2a8e21acc71e9e7fd3dd9
Dustin Dowell <dustindowell22@gmail.com>
2018-11-07 22:47:41 -0600

    spacing

--- src/app.js
+++ src/app.js
@@ -2,13 +2,10 @@
 'use strict'
 
 import { app } from 'hyperapp'
-
 import App from './views/App'
 
 const state = {}
-
 const actions = {}
-
 const view = s => App
 
 app(state, actions, view, document.getElementById('app'))
